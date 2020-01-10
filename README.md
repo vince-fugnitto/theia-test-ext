3d41740a1f0aa0366e861eff9182e8041bf777f6
vince-fugnitto <vincent.fugnitto@ericsson.com>
2020-01-10 08:54:39 -0500

    Initial Commit
    - created a sample VS Code extension which creates a file
    with content when the command `hello-world` is triggered.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# TEST VS CODE EXT FOR PR [#6851](https://github.com/eclipse-theia/theia/pull/6851)
+
+- Creates a file `test.txt` under the `workspaceRoot` with content when the command `hello world` is triggered.
---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+import { writeFileSync } from 'fs';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "test-ext" is now active!');
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		vscode.window.showInformationMessage('Creating File...');
+		let workspaceEdit = new vscode.WorkspaceEdit();
+		const workspacePath = vscode.workspace.workspaceFolders![0];
+		let pathUri = vscode.Uri.file(workspacePath.uri.fsPath + '/test.txt');
+		workspaceEdit.createFile(pathUri);
+		vscode.workspace.applyEdit(workspaceEdit).then(() => {
+			writeFileSync(pathUri.fsPath, 'TEST CONTENT\n');
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
